bce5c15aea37325af698c8c89a64e2cab92122a9
Misliani <misliani1620@gmail.com>
2024-09-21 18:26:37 +0800

    Add files via upload

---
+++ Salam.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package halo;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Salam {
+
+    public static void main(String[] args) {
+        String negara = "Indonesia";
+        int jmlProv = 38;
+
+        System.out.println("Halo " + negara);
+        System.out.println("Jumlah provinsi anda adalah " + jmlProv);
+    }
+
+}
